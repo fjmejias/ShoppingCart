880d85f2c5a25d0559192b0031d8b9a2e4907e94
fjma <fjmejiasarcos@gmail.com>
2017-03-22 10:32:31 +0000

    Delete WebUI

--- ShoppingCart.WebUI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace ShoppingCart.WebUI.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
